3a3581c5f674fd5eca11ae004816bff7b29f94e8
Dominick LoBraico <dj@lobraico.com>
2011-11-29 13:31:25 -0600

    adds new graphs

--- analysis/graphs_final/analysis.R
+++ analysis/graphs_final/analysis.R
@@ -7,14 +7,6 @@ speed$megapixels = speed$total_pixels/1000000
 speed$total_threads = speed$xthreads * speed$ythreads
 levels(speed$total_threads) =  c(1,4,16,64,144,256,400,484) 
 
-p <- ggplot(speed, aes(megapixels, legend = TRUE)) 
-p <- p +  opts(title = "Performance of Raytracer Implementations at Various Resolutions", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1))
-p <- p + geom_jitter(aes(y = speed$real_time, shape = application_type, fill = factor(total_threads)), position = position_jitter(width=7)) + scale_shape("Run", solid=FALSE) + scale_color_hue("Application Type")
-p <- p + scale_y_continuous("Render Time (ms)", formatter="comma", breaks = c(10000,60000,120000,180000,300000,420000)) 
-p <- p + scale_x_continuous("Megapixels Rendered", formatter="comma", breaks = c(1,10,50,100,200,300,400,500)) 
-
-ggsave("cumulative.pdf")
-
 threads_1   <- subset(speed,total_threads==1)
 threads_4   <- subset(speed,total_threads==4)
 threads_16  <- subset(speed,total_threads==16)
@@ -24,17 +16,84 @@ threads_256 <- subset(speed,total_threads==256)
 threads_400 <- subset(speed,total_threads==400)
 threads_484 <- subset(speed,total_threads==484)
 
-p <- ggplot(threads_1, aes(megapixels, legend = TRUE)) 
-p <- p +  opts(title = "Run 1", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1))
-p <- p + geom_jitter(aes(y = threads_1$render_time, fill = application_type), position = position_jitter(width=10))  
-p <- p + scale_y_continuous("Render Time (ms)", formatter="comma", breaks = c(10000,60000,120000,180000,300000,420000)) 
-p <- p + scale_x_continuous("Megapixels Rendered", formatter="comma", breaks = c(1,10,50,100,200,300,400,500)) 
-ggsave("threads_1.pdf")
+p <- ggplot(speed, aes(megapixels, legend = TRUE)) +  
+opts(title = "Performance of Raytracer Implementations at Various Resolutions and Block Sizes", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1)) + 
+geom_point(aes(y = real_time, shape = application_type, color = factor(total_threads)), size = 4) + scale_shape("Application Type", solid=TRUE) + scale_color_hue("Total Threads") + 
+scale_y_continuous("Real Time (ms)", formatter="comma") +
+scale_x_continuous("Megapixels Rendered", formatter="comma") 
+
+p
+ggsave("real_time.pdf")
+
+p + 
+scale_y_continuous("Real Time (ms)", formatter="comma", limits = c(0, 200000))
+ggsave("real_time_zoom1.pdf")
+
+p + 
+scale_y_continuous("Real Time (ms)", formatter="comma", limits = c(0, 60000))
+ggsave("real_time_zoom2.pdf")
+
+p +
+scale_y_continuous("Real Time (ms)", formatter="comma", limits = c(0, 18500)) + scale_x_continuous("Megapixels Rendered", formatter="comma", limits = c(0,25)) + geom_line(aes(y = real_time, group = factor(total_threads), color = factor(total_threads)))
+ggsave("real_time_zoom3.pdf")
+
+p <- ggplot(speed, aes(megapixels, legend = TRUE)) +  
+opts(title = "Performance of Raytracer Implementations at Various Resolutions and Block Sizes", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1)) + 
+geom_point(aes(y = render_time, shape = application_type, color = factor(total_threads), size = factor(total_threads)), position=position_jitter(h=0,w=8)) + scale_shape("Application Type", solid=TRUE) + scale_color_hue("Total Threads") + scale_size_manual("Total Threads", values=c(2,3,4,8,10,12,14,16,5)) + 
+scale_y_continuous("Render Time (ms)", formatter="comma") +
+scale_x_continuous("Megapixels Rendered with Horizontal Jitter", formatter="comma") 
+ggsave("render_time.pdf")
+
+p + 
+scale_y_continuous("Real Time (ms)", formatter="comma", limits = c(0, 100000))
+ggsave("render_time_zoom.pdf")
+
+p <- ggplot(speed, aes(megapixels, legend = TRUE)) +  
+opts(title = "Performance of Raytracer Implementations at Various Resolutions and Block Sizes", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1)) + 
+geom_point(aes(y = render_time, shape = application_type, color = factor(total_threads)), size = 4) + 
+scale_shape("Application Type", solid=TRUE) + 
+scale_color_hue("Total Threads") + 
+scale_x_continuous("Megapixels Rendered", formatter="comma", limits = c(0,65)) + 
+scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0, 50000)) +
+geom_smooth(aes(y = render_time, group = factor(total_threads), color = factor(total_threads), method="lm", fullrange=TRUE))
+p
+ggsave("render_time_no_jitter_with_line.pdf")
+
+p <- ggplot(speed, aes(megapixels, legend = TRUE)) +  
+opts(title = "Performance of Raytracer Implementations at Various Resolutions and Block Sizes", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1)) + 
+geom_point(aes(y = render_time, shape = application_type, color = factor(total_threads)), size = 4) + 
+scale_shape("Application Type", solid=TRUE) + 
+scale_color_hue("Total Threads") + 
+scale_x_continuous("Megapixels Rendered", formatter="comma", limits = c(0,1.0)) + 
+scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0, 1000)) #+
+#geom_smooth(aes(y = render_time, group = factor(total_threads), method="lm", fullrange=TRUE))
+p
+ggsave("render_time_no_jitter_zoom1.pdf")
+
+p +
+scale_x_continuous("Megapixels Rendered", formatter="comma", limits = c(0,0.25)) + 
+scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0, 200)) + 
+geom_line(aes(y = render_time, group = factor(total_threads), color = factor(total_threads)))
+ggsave("render_time_no_jitter_zoom2.pdf")
+
+p +
+scale_x_continuous("Megapixels Rendered", formatter="comma", limits = c(0,0.06)) + 
+scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0, 50)) + 
+geom_line(aes(y = render_time, group = factor(total_threads), color = factor(total_threads)))
+ggsave("render_time_no_jitter_zoom3.pdf")
 
 
-p <- ggplot(speed, aes(megapixels, legend = TRUE)) 
-p <- p +  opts(title = "Performance of Raytracer Implementations at Various Resolutions", legend.title = theme_text(colour='black', size=10, face='bold', hjust=-.1))
-p <- p + geom_jitter(aes(y = speed$render_time, fill = factor(total_threads), shape = application_type), position = position_jitter(width=7)) + scale_shape("Run", solid=FALSE) + scale_color_hue("Application Type")
-p <- p + scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0,10000)) 
-p <- p + scale_x_continuous("Megapixels Rendered", formatter="comma", breaks = c(1,2,3,4,5,6,7,8,9,10), limits = c(0,10))
-ggsave("inset.pdf")
+p <- ggplot(speed, aes(real_time, legend = TRUE)) +
+opts(title = "Render Time vs. Real Time", legend.title = theme_text(colour = 'black', size=10, face='bold', hjust=-.1)) +
+geom_point(aes(y=render_time, color = factor(total_threads), shape = application_type), size = 7) +
+scale_y_continuous("Render Time (ms)", formatter="comma") +
+scale_x_continuous("Real Time (ms)", formatter="comma") +
+scale_color_hue("Total Threads") + scale_shape("Application Type", solid=FALSE)
+p
+ggsave("render_time_vs_real_time.pdf")
+
+p + scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0,50000)) + scale_x_continuous("Real Time (ms)", formatter="comma", limits = c(0,50000))
+ggsave("render_time_vs_real_time_zoom1.pdf")
+
+p + scale_y_continuous("Render Time (ms)", formatter="comma", limits = c(0,1500)) + scale_x_continuous("Real Time (ms)", formatter="comma", limits = c(0,6000))
+ggsave("render_time_vs_real_time_zoom2.pdf")
